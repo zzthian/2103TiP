d27ef84482835a5b8b232a80fd6bf5ef6157e824
zzthian <zzthian@hotmail.com>
2022-08-19 14:40:30 +0800

    Level-2
    implemented list capability

--- src/main/java/Tracker.java
+++ src/main/java/Tracker.java
@@ -3,20 +3,30 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 public class Tracker {
+    public static List<String> list = new ArrayList<>();
 
     public Tracker() {
+    }
+
+    public void printList() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i+1) +". " + list.get(i));
+        }
     }
 
     public void simulate() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                printList();
+            } else {
+                System.out.println("added: " + input);
+                list.add(input);
 
-            System.out.println(input);
-            input = sc.nextLine();
             }
-
-
+            input = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
         sc.close();
     }
